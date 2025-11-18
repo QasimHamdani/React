7c28c7d17f0a50b8631140ee1a0d68c8db57f627
qasimhamdani <qasim_hamdani26@milton.edu>
2025-11-18 12:19:24 -0500

    did stuff

--- src/Text.jsx
+++ src/Text.jsx
@@ -1,6 +1,7 @@
 function Text(){
     return(
-        <h1 style="color:rgba(64, 165, 113, 1);">Custom text component</h1>
+        <h1 >Custom text component</h1>
+        // <></>
     )
 }
 export default Text;
